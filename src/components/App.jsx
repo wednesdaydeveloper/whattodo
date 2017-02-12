bdeb9efddc897c304d26b5e8b9d57dc6171569b7
wednesday_developer <wednesday.developer@gmail.com>
2017-02-12 00:20:27 +0000

    動作するように修正。

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,8 @@
 import React from 'react';
-//import 'react-date-picker/index.css';
-//import { DateField, Calendar } from 'react-date-picker';
-
 import TodoAddContainer from '../containers/TodoAdd.js';
 import VisibleTodoList from '../containers/VisibleTodoList.js';
 import Footer from '../components/Footer.jsx';
 
-
-let date = '2017-04-24'
 
 const App = () => (
 	<div>
--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
-/*
-redux 無し todo
-http://javascript-memo2.seesaa.net/article/436858441.html
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
-http://qiita.com/erukiti/items/e16aa13ad81d5938374e
-*/
-
-
+import './index.css';
 import App from './components/App.jsx';
 import store from './Store.js';
 
-
-import { Provider } from 'react-redux';
-import {render} from 'react-dom';
-
-render(
+ReactDOM.render(
   <Provider store = { store } >
     <App />
   </Provider>,
